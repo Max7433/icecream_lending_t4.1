92d85702fb2aeec3ad2c7976fdbb18fb6f8dfbda
Max7433 <104869880+Max7433@users.noreply.github.com>
2022-08-12 07:17:38 +0000

    Deploying to gh-pages from @ Max7433/icecream_lending_t4.1@f4a05a4e6fec41793624702a78bfb59c64f6fa5d 🚀

---
+++ index.bc7f1c4f.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".customers__slider").slick({arrows:!1,dots:!0,speed:1500,autoplay:!0,autoplaySpeed:3500,centerMode:!0})}));
+//# sourceMappingURL=index.bc7f1c4f.js.map
--- index.d920b9d1.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$(".customers__slider").slick({arrows:!1,dots:!0})}));
-//# sourceMappingURL=index.d920b9d1.js.map
