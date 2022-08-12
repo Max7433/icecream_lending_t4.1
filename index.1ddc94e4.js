6458df33fd1bc58987261e3fad9ec1f6c96ef34b
Max7433 <104869880+Max7433@users.noreply.github.com>
2022-08-12 06:32:52 +0000

    Deploying to gh-pages from @ Max7433/icecream_lending_t4.1@eb7facfa0b5a688e5ee36a2c3a9435bc8f2dc29d 🚀

--- index.1ddc94e4.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-open-two]"),closeModalBtn:document.querySelector("[data-modal-close-two]"),modal:document.querySelector("[data-modal-two]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.1ddc94e4.js.map
---
+++ index.7e9db8d4.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open-twoP]"),closeModalBtn:document.querySelector("[data-modal-close-twoP]"),modal:document.querySelector("[data-modal-twoP]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.7e9db8d4.js.map
